3d86a198b489a37c788cd02c21abb1efa2d8c40e
santiago diaz <sidiaz4@miuandes.cl>
2021-05-08 13:31:03 -0400

    final

--- conta.js
+++ conta.js
@@ -1,19 +1,27 @@
 
-
-const prompt = require('prompt-sync')();
- 
 function view(counter){
-    return(`\ncounter: +${counter} \n (+)(-)\n`);
+    return(`\ncounter: ${counter} \n (+)(-)\n (q)\n`);
 }
 
 function update(msg, counter){
-
+    if (msg === '+') 
+        return counter = counter + 1;
+    else if (msg === '-')
+        return counter = counter - 1;
+    else if (msg === 'q')
+        return msg;
+    else
+        return counter;
 }
 
 function app(counter){
-
+    while (counter != 'q'){
+        const currentView = view(counter);
+        console.clear();
+        console.log(currentView);
+        const prompt = require('prompt-sync')();
+        const msg = prompt('What would you do? ');
+        counter = update(msg, counter);
+    }
 }
-
-readline.question("What would you do: ", msg => {
-    readline.close()
-});
+app(0);
